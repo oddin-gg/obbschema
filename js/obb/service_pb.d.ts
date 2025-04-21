5fd06beba96b1bc37cefb741f1496d8a717c1ff7
Adam Bíl <adam.bil@oddin.gg>
2025-04-21 12:29:22 +0200

    CORE-1971: [Changed] Session invalid argument comment to a more precise one.

--- js/obb/service_pb.d.ts
+++
@@ -1,8 +0,0 @@
-// package: obb
-// file: obb/service.proto
-
-import * as jspb from "google-protobuf";
-import * as obb_markets_pb from "../obb/markets_pb";
-import * as obb_popular_pb from "../obb/popular_pb";
-import * as obb_session_pb from "../obb/session_pb";
-
