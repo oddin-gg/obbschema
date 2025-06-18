4ef5557e95355517bbe2d201b3ed898905a7d1c3
Tomáš Štětina <tomas.stetina@oddin.gg>
2025-06-18 11:57:39 +0200

    Adding support for Linux

--- scripts/python_fix_imports.sh
+++ scripts/python_fix_imports.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+SED_INPLACE="$1"
+
 # protocol buffers wond do the update https://github.com/protocolbuffers/protobuf/issues/9667
 for file in ./proto/*
 do
-  dir="$(basename ${file})"
-  sed -i '' -E "s/from ${dir}/from obb_schema.${dir}/" ./python/**/*.py
+  dir="$(basename "${file}")"
+  find ./python -type f -name "*.py" -exec $SED_INPLACE "s/from ${dir}/from obb_schema.${dir}/" {} +
 done
